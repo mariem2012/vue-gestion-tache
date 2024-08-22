d58078b740549c14a21693ca11da0ee2afcd10f2
Ousmanly <lyousmane492@gmail.com>
2024-08-22 22:47:15 +0200

    added component ModifiListe

--- src/router/index.js
+++ src/router/index.js
@@ -3,13 +3,11 @@ import TacheView from '../views/TacheView.vue'
 import ProjetView from '../views/ProjetView.vue'
 import AjoutTache from '@/components/Taches/AjoutTache.vue'
 import ListTache from '@/components/Taches/ListTache.vue'
-<<<<<<< HEAD
 import AjoutProjet from '@/components/Projets/AjoutProjet.vue'
 import ListProjet from '@/components/Projets/ListProjet.vue'
 
-=======
+
 import ModifiTache from '@/components/Taches/ModifiTache.vue'
->>>>>>> 96da802 (added component ModifiListe)
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -30,18 +28,19 @@ const router = createRouter({
       component: ListTache
     },
     {
-<<<<<<< HEAD
+
       path: '/ajoutprojet',
       component: AjoutProjet
     },
     {
       path: '/listprojet',
-      component: ListProjet
-=======
+      component: ListProjet,
+    },
+    {
       path: '/modifietache',
       component: ModifiTache
->>>>>>> 96da802 (added component ModifiListe)
     }
+    
   ]
 })
 
